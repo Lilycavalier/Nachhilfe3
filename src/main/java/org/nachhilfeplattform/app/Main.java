8933560f9a75cd355a1057b865c7b630f63d7045
Jennyca-Shy <jessy.shi@gmx.de>
2026-06-11 11:41:33 +0200

    test

--- src/main/java/org/nachhilfeplattform/app/Main.java
+++ src/main/java/org/nachhilfeplattform/app/Main.java
@@ -11,3 +11,19 @@ public class Main {
 
     }
 }
+
+//java org.nachhilfeplattform.app.Main
+/*
+C:\Users\jessy\OneDrive\Desktop\Informatik\Nachhilfe3>javac org\nachhilfeplattform\app\Main.java      
+Fehler: Datei nicht gefunden: org\nachhilfeplattform\app\Main.java
+Verwendung: javac <Optionen> <Quelldateien>
+Mit --help können Sie eine Liste der möglichen Optionen aufrufen
+
+C:\Users\jessy\OneDrive\Desktop\Informatik\Nachhilfe3>cd src/main/java
+
+C:\Users\jessy\OneDrive\Desktop\Informatik\Nachhilfe3\src\main\java>javac org\nachhilfeplattform\app\Main.java
+
+C:\Users\jessy\OneDrive\Desktop\Informatik\Nachhilfe3\src\main\java>java org.nachhilfeplattform.app.Main
+
+C:\Users\jessy\OneDrive\Desktop\Informatik\Nachhilfe3\src\main\java>
+*/
