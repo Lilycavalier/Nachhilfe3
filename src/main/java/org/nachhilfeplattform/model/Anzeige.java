5693ce765fbc6294b78768ff24f202bc0b9bb359
Lilycavaliers <lilykrie@woodland.on.ca>
2026-06-11 11:52:21 +0200

    model fertig

--- src/main/java/org/nachhilfeplattform/model/Anzeige.java
+++ src/main/java/org/nachhilfeplattform/model/Anzeige.java
@@ -4,41 +4,60 @@ public class Anzeige {
 
     private String name;
     private String klassenstufe;
-    private String passwort;
     private String kontaktdaten;
     private String faecher;
     private String zeit;
+    private String beschreibung;
 
-    public Anzeige(String name, String klassenstufe, String passwort, String kontaktdaten, String faecher, String zeit, boolean login)
+    public Anzeige(String name, String klassenstufe, String kontaktdaten, String faecher, String zeit, String beschreibung, boolean login)
     {
         this.name = name;
         this.klassenstufe = klassenstufe;
-        this.passwort = passwort;
         this.kontaktdaten = kontaktdaten;
         this.faecher = faecher;
         this.zeit = zeit;
+        this.beschreibung = beschreibung;
 
     }
 
     public void datenAusgeben(){
         System.out.println("Name: " + name);
         System.out.println("Klassenstufe: " + klassenstufe);
-        System.out.println("Passwort: " + passwort);
         System.out.println("Kontaktdaten:"+kontaktdaten);
         System.out.println("Fächer" + faecher);
         System.out.println("Zeit"+ zeit);
+        System.out.println("Beschreibung"+ beschreibung);
+
     }
 
     public void anzeigeErstellen() {
-
+        System.out.println("Anzeige wurde erstellt");
+        datenAusgeben();
     }
 
     public void anzeigeLöschen(){
+        name = "";
+        klassenstufe = "";
+        kontaktdaten = "";
+        faecher = "";
+        zeit = "";
+        beschreibung = "";
+
+        System.out.println("Anzeige wurde gelöscht");
+
 
     }
 
 
-    public void anzeigeBearbeiten(){
+    public void anzeigeBearbeiten(String neueFaecher, String neueZeit, String neueKlassenstufe, String neueBeschreibung, String neueKontaktdaten){
+
+        this.klassenstufe = neueKlassenstufe;
+        this.kontaktdaten = neueKontaktdaten;
+        this.faecher = neueFaecher;
+        this.zeit = neueZeit;
+        this.beschreibung = neueBeschreibung;
+
+        System.out.println("Anzeige wurde bearbeitet");
 
     }
 }
